cf54b30e2487190dd00991718c8de8c262c62d5a
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-14 13:34:06 -0300

    ter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import "./App.css";
-import Cards from "./components/Cards";
-import Navbar from "./components/Navbar";
-import Carousel from "./components/Carousel";
-import Footer from "./components/Footer";
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HOLA</h1>
-      <h2>CHAO</h2>
-      <Navbar />
-      <Carousel />
-      <Cards />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
